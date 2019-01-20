3341b9bd6badbad087273a41f5ef1b754238f4f6
Lucas Tong <0xDECAFC0FFEE@gmail.com>
2019-01-20 08:04:33 -0800

    blacklist with weights updated each epoch stable - unconvinced this increases accuracy. pruning of >90% of edges is probably possible without decreases in accuracy

---
+++ deep_model.py
@@ -0,0 +1,110 @@
+from collections import namedtuple
+import numpy as np
+import tensorflow as tf
+
+LayerParam = namedtuple("ModelLayerParams", ["type", "params", "init"])
+LayerParam.__new__.__defaults__ = (None, None, None)
+
+def set_layer_param_init_values(layers_params):
+    prev_layer_size = None
+    prev_layer_output = None
+    for i, layer_params in enumerate(layers_params):
+        layer_type, params, init = layer_params
+        if layer_type == "input":
+            prev_layer_size = params
+        elif layer_type == "dropout":
+            pass
+        elif layer_type in ["dense", "output"]:
+            layer_size = params
+            W_shape = [prev_layer_size, layer_size]
+            b_shape = [layer_size]
+            W = tf.Session().run(tf.contrib.layers.xavier_initializer()(W_shape))
+            b = tf.Session().run(tf.contrib.layers.xavier_initializer()(b_shape))
+            layers_params[i] = list(layer_params)
+            assert layers_params[i][2] == None, "second element of layer params should be init values"
+            layers_params[i][2] = (W, b)
+            layers_params[i] = LayerParam(*layers_params[i])
+            prev_layer_size = layer_size
+        else:
+            raise Exception("didnt find layer type", layer_type)
+
+def relu_layer(X, prev_layer_size, layer_size, init):
+    with tf.name_scope('relu_node'):
+        W_shape = [prev_layer_size, layer_size]
+        b_shape = [layer_size]
+        W, b = init
+        W = tf.Variable(initial_value=W)
+        b = tf.Variable(initial_value=b)
+        return tf.nn.relu(tf.matmul(X, W) + b), (W, b)
+
+def softmax_layer(X, prev_layer_size, layer_size, init):
+    with tf.name_scope('softmax_node'):
+        W_shape = [prev_layer_size, layer_size]
+        b_shape = [layer_size]
+        W, b = init
+        W = tf.Variable(initial_value=W)
+        b = tf.Variable(initial_value=b)
+        return tf.nn.softmax(tf.matmul(X, W) + b), (W, b)
+
+
+def build_deep_model(layer_parameters):
+    x = None # will be set in the init layer
+    y_true = tf.placeholder(tf.float32, [None, layer_parameters[-1].params])
+    prev_layer_size = None
+    prev_layer_output = None
+    dnn_variables = []
+    for layer_params in layer_parameters:
+        layer_type, params, init = layer_params
+        if layer_type == "input":
+            num_inputs = params
+            x = tf.placeholder(tf.float32, [None, num_inputs]) # equivalent to x
+            prev_layer_output = x
+            prev_layer_size = num_inputs
+            variables = []
+        elif layer_type == "dropout":
+            rate = params
+            prev_layer_output = tf.layers.dropout(prev_layer_output, rate)
+            variables = []
+        elif layer_type == "dense":
+            layer_size = params
+            prev_layer_output, variables = relu_layer(prev_layer_output, prev_layer_size, layer_size, init)
+            prev_layer_size = layer_size
+        elif layer_type == "output":
+            layer_size = params
+            prev_layer_output, variables = softmax_layer(prev_layer_output, prev_layer_size, layer_size, init)
+            prev_layer_size = layer_size
+        else:
+            raise Exception("didnt find layer type", layer_type)
+        dnn_variables.append(variables)
+
+    return x, prev_layer_output, y_true, dnn_variables
+
+def build_blacklist(layers_params):
+    prev_layer_size = None
+    prev_layer_output = None
+    num_weights = 0
+    blacklists = []
+    for layer_params in layers_params:
+        layer_type, params, init = layer_params
+        if layer_type == "input":
+            prev_layer_size = params
+            blacklists.append([])
+        elif layer_type == "dropout":
+            blacklists.append([])
+        elif layer_type in ["dense", "output"]:
+            layer_size = params
+            W_blacklist = np.full(shape=(prev_layer_size, layer_size), fill_value=True)
+            b_blacklist = np.full(shape=(layer_size), fill_value=True)
+            blacklists.append((W_blacklist, b_blacklist))
+            num_weights += prev_layer_size*layer_size
+            num_weights += layer_size
+            prev_layer_size = layer_size
+        else:
+            raise Exception("didnt find layer type", layer_type)
+
+    return blacklists, num_weights
+
+def update_blacklist(blacklists, blacklisted_index):
+    layer, wb, index_list = blacklisted_index
+    blacklists[layer][wb][index_list] = False
+    return blacklists
--- tensorflow_test.py
+++ tensorflow_test.py
@@ -9,7 +9,8 @@ from sklearn.metrics import confusion_matrix
 import copy
 import matplotlib.pyplot as plt
 from collections import namedtuple
-import time
+
+from deep_model import LayerParam, set_layer_param_init_values, build_deep_model, build_blacklist, update_blacklist
 
 def batchify(Xs, ys, batches):
     Xs, ys = shuffle(Xs, ys, random_state=0)
@@ -20,353 +21,128 @@ def batchify(Xs, ys, batches):
 def mnist_dataset():
     raw_data = tf.keras.datasets.mnist.load_data(path='mnist.npz')
     train_X, train_y_raw = raw_data[0]
-    test_X, test_y_raw = raw_data[1]
+    val_X, val_y_raw = raw_data[1]
 
     train_X = train_X.reshape((len(train_X), 784))
-    test_X = test_X.reshape((len(test_X), 784))
+    val_X = val_X.reshape((len(val_X), 784))
 
     # normalizing x values in [0, 1]
     train_X = train_X/255.0
-    test_X = test_X/255.0
-
-    train_X = np.random.rand(len(train_X), 7)
-    test_X = np.random.rand(len(test_X), 7)
+    val_X = val_X/255.0
 
     # 1 hot encoding y values
     train_y = np.zeros((len(train_y_raw), 10))
     train_y[np.arange(len(train_y_raw)), train_y_raw] = 1
-    test_y = np.zeros((len(test_y_raw), 10))
-    test_y[np.arange(len(test_y_raw)), test_y_raw] = 1
+    val_y = np.zeros((len(val_y_raw), 10))
+    val_y[np.arange(len(val_y_raw)), val_y_raw] = 1
 
-    return (train_X, train_y), (test_X, test_y)
+    return (train_X, train_y), (val_X, val_y)
 
-def build_model(num_inputs, layer_parameters, layer_def_repository):
-    x = tf.placeholder(tf.float32, [None, num_inputs])
+WeightIndex = namedtuple("WeightIndex", ["weight", "layer", "wb", "index"])
 
-    prev_layer_size = num_inputs
-    prev_layer_output = x
-    variables_list = []
+def get_layer_weights(sess, dnn_variables):
+    def get_weights(i_l, i_wb, i_cur, weights):
+        if len(weights.shape) > 1:
+            weight_indices = []
+            for i, weight in enumerate(weights):
+                c_weights = get_weights(i_l, i_wb, i_cur+(i,), weight)
+                weight_indices.extend(c_weights)
+            return weight_indices
+        elif len(weights.shape) == 1:
+            return [WeightIndex(weight, i_l, i_wb, i_cur+(i,)) for i, weight in enumerate(weights)]
+        else:
+            raise Exception("unexpected weights value %s" % weighs)
 
-    for layer_desc in layer_parameters:
-        layer_type, params, initial_values, blacklist = layer_desc
-        if layer_type == "dropout":
-            rate = params
-            prev_layer_output = tf.layers.dropout(prev_layer_output, rate)
-            variables_list.append((layer_type, []))
-        elif layer_type in {"dense", "softmax"}:
-            layer_size = params
-            prev_layer_output, variable = layer_def_repository[layer_type](prev_layer_output, prev_layer_size, layer_size, initial_values, blacklist)
-            variables_list.append((layer_type, variable))
-        prev_layer_size = layer_size
+    variables = []
+    for i_l, layer in enumerate(dnn_variables):
+        for i_wb, weight_variables in enumerate(layer):
+            weight_values = sess.run(weight_variables)
+            variables.extend(get_weights(i_l, i_wb, tuple(), weight_values))
 
-    last_layer_type, num_outputs, __, __ = layer_parameters[-1]
-    y_true = tf.placeholder(tf.float32, [None, num_outputs])
-    y = prev_layer_output
+    return variables
 
-    return x, y, y_true, variables_list
+def experiment(layers_params, blacklisted):
+    (train_X, train_y), (val_X, val_y) = mnist_dataset()
 
+    x, y, y_true, dnn_variables = build_deep_model(layers_params)
 
-def get_initial_weights(layer_parameters):
-    # wrote this whole function cause don't want to write xavier initialization wrong.
-    # copies a bunch of code from create deep model 
-    print(layer_parameters)
-    first_layer = layer_parameters.pop(0)
-    layer_1_type, num_inputs = first_layer.type, first_layer.param
-    assert(layer_1_type == "input")
-
-    def sigmoid_layer(X, prev_layer_size, layer_size, initial_weights, blacklist):
-        W = tf.get_variable("W_sigmoid", shape=[prev_layer_size, layer_size], initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.get_variable("b_sigmoid", shape=[layer_size], initializer=tf.contrib.layers.xavier_initializer())
-        return tf.nn.relu(tf.matmul(X, W) + b), (W, b)
-
-    def softmax_layer(X, prev_layer_size, num_outputs, initial_weights, blacklist):
-        W = tf.get_variable("W_softmax", shape=[prev_layer_size, num_outputs], initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.get_variable("b_softmax", shape=[num_outputs], initializer=tf.contrib.layers.xavier_initializer())
-        return tf.nn.softmax(tf.matmul(X, W) + b), (W, b)
-    layer_def_repository = {"softmax": softmax_layer, "dense": sigmoid_layer}
-
-    x, y, y_true, variables_list = build_model(num_inputs, layer_parameters, layer_def_repository)
-
-    # initialize weights
+    loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(y_true, y))
+    train_step = tf.train.AdamOptimizer(learning_rate=.01).minimize(loss)
     init = tf.global_variables_initializer()
 
-    init_layer_weights_list = []
-    blacklist = []
+    # tpu_address = 'grpc://' + os.environ['COLAB_TPU_ADDR']
+    # with tf.Session(tpu_address) as sess:
     with tf.Session() as sess:
+        acc_over_time = []
+
         sess.run(init)
-        for (layer_type, variables) in variables_list:
-            if layer_type in {"softmax", "dense"}:
-                W_variable, b_variable = variables
-                W_variable = sess.run(W_variable)
-                b_variable = sess.run(b_variable)
-                init_layer_weights_list.append((W_variable, b_variable))
-                blacklist.append(np.full(W_variable.shape, True))
-            else:
-                init_layer_weights_list.append(None)
-                blacklist.append(None)
-    
-    return init_layer_weights_list, blacklist
+        layer_weights = get_layer_weights(sess, dnn_variables)
 
-def build_deep_model_with_init_blacklist(layer_parameters):
-    
-    first_layer = layer_parameters.pop(0)
-    layer_1_type, num_inputs = first_layer.type, first_layer.param
-    assert(layer_1_type == "input")
+        batches = 8
+        batch_size = int(len(train_X)/batches)
 
-    def sigmoid_layer(X, prev_layer_size, layer_size, init, blacklist):
-        W_init, b_init = init
-        W = np.empty(shape=[prev_layer_size, layer_size], dtype=tuple)
-        b = np.empty(shape=[layer_size], dtype=tuple)
-
-        for layer_index in tqdm(range(layer_size)):
-            b[layer_index] = tf.Variable(b_init[layer_index])wa
-            for column_index in range(prev_layer_size):
-                W[column_index][layer_index] = tf.Variable(W_init[column_index][layer_index])
-
-        for layer_index in tqdm(range(layer_size)):
-            bp = b[layer_index]
-            for column_index in range(prev_layer_size):
-                if blacklist[column_index][layer_index] == False:
-                    bp = bp + (W[column_index][layer_index] * X[column_index])
-            b[layer_index] = bp
-
-        return tf.nn.relu(tf.stack(list(b)), (W, b))
-
-    def softmax_layer(X, prev_layer_size, layer_size, init, blacklist):
-        W_init, b_init = init
-        W = np.empty(shape=[prev_layer_size, layer_size], dtype=tf.Variable)
-        b = np.empty(shape=[layer_size], dtype=tf.Variable)
-
-        for layer_index in tqdm(range(layer_size)):
-            b[layer_index] = tf.Variable(b_init[layer_index])
-            for column_index in range(prev_layer_size):
-                W[column_index][layer_index] = tf.Variable(W_init[column_index][layer_index])
-
-        for layer_index in tqdm(range(layer_size)):
-            bp = b[layer_index]
-            for column_index in range(prev_layer_size):
-                # if blacklist[column_index][layer_index] != False:
-                    bp = bp + W[column_index][layer_index] * X[column_index]
-            b[layer_index] = bp
-        return tf.nn.softmax(tf.stack(list(b)), (W, b))
-
-    layer_def_repository = {"dense": sigmoid_layer, "softmax": softmax_layer}
-
-    return build_model(num_inputs, layer_parameters, layer_def_repository)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(train_X, train_y), (test_X, test_y) = mnist_dataset()
-
-MyLayer = namedtuple("MyLayer", ["type", "param", "init", "blacklist"])
-MyLayer.__new__.__defaults__ = (None, None, None, None)
-
-model_parameters = [
-    MyLayer("input", 7), #MyLayer("input", 784),
-    MyLayer("dense", 512), 
-    MyLayer("dropout", .2), 
-    MyLayer("softmax", 10)
-]
-# last two inputs to each layer is the blacklist and the initial values
-
-initial_weights, blacklist = get_initial_weights(copy.deepcopy(model_parameters))
-
-
-for index, (parameter, init_weight, bl) in enumerate(zip(model_parameters[1:], initial_weights, blacklist)):
-    l_type, param = parameter.type, parameter.param
-    model_parameters[index+1] = MyLayer(l_type, param, init_weight, bl)
-
-x, y, y_true, variables_list = build_deep_model_with_init_blacklist(copy.deepcopy(model_parameters))
-compile_start_time = time.time()
-print("started compiling", compile_start_time)
-
-loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(y_true, y))
-train_step = tf.train.AdamOptimizer(learning_rate=.01).minimize(loss)
-init = tf.global_variables_initializer()
-
-compile_end_time = time.time()
-print("sess starting", compile_end_time, compile_end_time-compile_start_time)
-
-# training
-with tf.Session() as sess:
-    sess.run(init)
-
-    print("sess initted")
-
-    acc_over_time = []
-    batches = 8
-    batch_size = int(len(train_X)/batches)
-
-    for epoch in tqdm(range(10)): #200
-        for Xs, ys in batchify(train_X, train_y, batches):
-            # Train
-            sess.run(train_step, feed_dict={ x: Xs, y_true: ys})
+        for epoch in range(200): #200
+            for Xs, ys in batchify(train_X, train_y, batches):
+                # Train
+                sess.run(train_step, feed_dict={ x: Xs, y_true: ys})
+                for variable_layer, blacklisted_layer in zip(dnn_variables, blacklisted):
+                    for variable, blacklisted_wb in zip(variable_layer, blacklisted_layer):
+                        variable.load(np.where(blacklisted_wb, sess.run(variable), 0), sess)
 
         # check test acc
-        pred_ys_max_index = np.argmax(sess.run(y, feed_dict={x: test_X}), axis=1)
-        test_ys_max_index = np.argmax(test_y, axis=1)
+            pred_ys_max_index = np.argmax(sess.run(y, feed_dict={x: val_X}), axis=1)
+            val_ys_max_index = np.argmax(val_y, axis=1)
 
-        test_acc = list(map(lambda x: x[0] == x[1], zip(pred_ys_max_index, test_ys_max_index)))
-        test_acc = np.count_nonzero(test_acc)/float(len(test_acc))
-        print("test test_acc", test_acc)
-        acc_over_time.append(test_acc)
+            val_acc = list(map(lambda x: x[0] == x[1], zip(pred_ys_max_index, val_ys_max_index)))
+            val_acc = np.count_nonzero(val_acc)/float(len(val_acc))
+            acc_over_time.append(val_acc)
 
-    print(confusion_matrix(test_ys_max_index, pred_ys_max_index))
-    plt.plot(acc_over_time)
-    plt.show()
+        layer_weights = get_layer_weights(sess, dnn_variables)
+        return acc_over_time, layer_weights
 
+init_layers_params = [
+    LayerParam(type="input", params=784),
+    LayerParam(type="dense", params=512), 
+    LayerParam(type="dropout", params=.2), 
+    LayerParam(type="output", params=10)
+]
+set_layer_param_init_values(init_layers_params)
+blacklisted, num_weights = build_blacklist(init_layers_params)
+num_weights_initial = num_weights
 
+prune_percent = .2
+accs_over_time = []
 
+for experiment_num in tqdm(range(3)):
+    layers_params = copy.deepcopy(init_layers_params)
+    for layer_params, layer_blacklist in zip(layers_params, blacklisted):
+        if layer_params.init == None:
+            continue
+        for layer_param, layer_blacklisted_wb in zip(layer_params.init, layer_blacklist):
+            layer_param = np.where(layer_blacklisted_wb, layer_param, 0)
 
+    acc_over_time, weight_indices = experiment(layers_params, blacklisted)
+    accs_over_time.append((acc_over_time, num_weights/num_weights_initial * 100))
+    print("experiment %s final acc %s %s%% network weights left" % (experiment_num, acc_over_time[-1], num_weights/num_weights_initial *100))
+    weight_indices.sort(key=lambda x: abs(x[0]))
 
+    prune_num = (num_weights)*prune_percent
+    for weight_index in weight_indices:
+        weight, layer, wb, index = weight_index
 
+        if blacklisted[layer][wb][index] == True:
+            blacklisted[layer][wb][index] = False
+            num_weights -= 1
+            prune_num -= 1
+            if prune_num < 1:
+                break
 
+lines = []
+fig, ax = plt.subplots()
+for expr_num, (acc_over_time, network_left) in enumerate(accs_over_time):
+    color = expr_num/float(len(accs_over_time))
+    line = ax.plot(acc_over_time, label="%s%%" % network_left, color=(0, color, 0), linewidth=.2)
 
-
-
-
-# def build_deep_model(model_dimensions, initial_values, blacklist):
-#     num_inputs, layer_parameters, num_outputs = model_dimensions
-#     assert len(layer_parameters)+1 == len(initial_values)
-
-#     x = tf.placeholder(tf.float32, [None, num_inputs])
-#     y_true = tf.placeholder(tf.float32, [None, num_outputs])
-
-#     def sigmoid_layer(X, prev_layer_size, layer_size, initial_value):
-#         if initial_value == "xavier":
-#             W = tf.get_variable("W", shape=[prev_layer_size, layer_size], initializer=tf.contrib.layers.xavier_initializer())
-#             b = tf.get_variable("b", shape=[layer_size], initializer=tf.contrib.layers.xavier_initializer())
-#         else:
-#             # W = np.a
-#             # for layer in range(layer_size):
-#             #     for weight in range(prev_layer_size):
-
-#             W = tf.get_variable("W", shape=[prev_layer_size, layer_size], initializer=tf.constant(initial_value[0]))
-#             b = tf.get_variable("b", shape=[layer_size], initializer=tf.constant(initial_value[1]))
-#         return tf.nn.relu(tf.matmul(X, W) + b), (W, b)
-
-
-#     def softmax_layer(X, prev_layer_size, num_outputs, initial_value):
-#         with tf.name_scope('softmax_node'):
-#             if initial_value == "xavier":
-#                 W = tf.get_variable("W2", shape=[prev_layer_size, num_outputs], initializer=tf.contrib.layers.xavier_initializer())
-#                 b = tf.get_variable("b2", shape=[num_outputs], initializer=tf.contrib.layers.xavier_initializer())
-#             else:
-#                 W = tf.get_variable("W2", shape=[prev_layer_size, num_outputs], initializer=tf.constant(initial_value[0]))
-#                 b = tf.get_variable("b2", shape=[num_outputs], initializer=tf.constant(initial_value[1]))
-#             return tf.nn.softmax(tf.matmul(X, W) + b), (W, b)
-
-#     prev_layer_size = num_inputs
-#     prev_layer_output = x
-#     variables_list = []
-#     for layer_desc, initial_value in zip(layer_parameters, initial_values):
-#         layer_type, params = layer_desc
-#         if layer_type == "dropout":
-#             rate = params
-#             prev_layer_output = tf.layers.dropout(prev_layer_output, rate)
-#             variables_list.append([])
-#         elif layer_type == "dense":
-#             layer_size = params
-#             prev_layer_output, variable = sigmoid_layer(prev_layer_output, prev_layer_size, layer_size, initial_value)
-#             prev_layer_size = layer_size
-#             variables_list.append(variable)
-
-#     y, variable = softmax_layer(prev_layer_output, prev_layer_size, num_outputs, initial_values[-1])
-#     variables_list.append(variable)
-
-#     return x, y, y_true, variables_list
-
-
-
-
-
-
-
-
-
-
-
-
-# initial_values = ["xavier", "", "xavier"]
-# x, y, y_true, variables_list = build_deep_model(model_dimensions, initial_values)
-
-# # categorical crossentropy
-# loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(y_true, y))
-
-# train_step = tf.train.AdamOptimizer(learning_rate=.01).minimize(loss)
-
-# init = tf.global_variables_initializer()
-
-# # tpu_address = 'grpc://' + os.environ['COLAB_TPU_ADDR']
-# # with tf.Session(tpu_address) as sess:
-
-
-# blacklist_variables_list = []
-
-# # getting initial variable weights
-# initial_weights_list = []
-# with tf.Session() as sess:
-#     sess.run(init)
-#     for variables in variables_list:
-#         initial_weights = []
-#         for variable in variables:
-#             initial_weights.append(sess.run(variable))
-        
-
-
-
-
-
-
-# for prune_num in range(0):
-
-
-
-
-#     with tf.Session() as sess:
-#         acc_over_time = []
-#         batches = 8
-#         batch_size = int(len(train_X)/batches)
-
-#         for epoch in tqdm(range(10)): #200
-#             for Xs, ys in batchify(train_X, train_y, batches):
-#                 # Train
-#                 sess.run(train_step, feed_dict={ x: Xs, y_true: ys})
-
-#         # check test acc
-#             pred_ys_max_index = np.argmax(sess.run(y, feed_dict={x: test_X}), axis=1)
-#             test_ys_max_index = np.argmax(test_y, axis=1)
-
-#             test_acc = list(map(lambda x: x[0] == x[1], zip(pred_ys_max_index, test_ys_max_index)))
-#             test_acc = np.count_nonzero(test_acc)/float(len(test_acc))
-#             print("test test_acc", test_acc)
-#             acc_over_time.append(test_acc)
-
-#         print(confusion_matrix(test_ys_max_index, pred_ys_max_index))
-#         plt.plot(acc_over_time)
-#         plt.show()
-
-#         print("===========final weights=================")
-#         for variables in variables_list:
-#             for variable in variables:
-#                 print(variable)
-#                 print(sess.run(variable))
-
-
-
-
-
-
+ax.legend()
+plt.title("blacklist with weights updated each epoch")
+plt.show()
